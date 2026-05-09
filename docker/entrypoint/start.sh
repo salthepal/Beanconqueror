7131ecd14d6711270f5072e043494db4f4f92fd9
Salil Phadnis <41760527+salthepal@users.noreply.github.com>
2026-05-09 10:59:53 -0500

    Fix runtime token wiring and remove legacy API token headers

--- docker/entrypoint/start.sh
+++ docker/entrypoint/start.sh
@@ -3,6 +3,7 @@ set -eu
 
 export API_BASE_URL="${API_BASE_URL:-/api}"
 export API_PORT="${API_PORT:-3000}"
+export API_CLIENT_TOKEN="${API_CLIENT_TOKEN:-}"
 if [ -z "${FEATURE_FLAGS_JSON:-}" ]; then
   FEATURE_FLAGS_JSON='{}'
 fi
